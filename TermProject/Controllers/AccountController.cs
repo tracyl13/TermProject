dae7ce2230f93fcb343081b5d7d0d374e03c99e4
Tracy <tlant300@gmail.com>
2025-04-03 20:21:08 -0400

    added Views and set up html tags, added Controllers and set up the Views they return, added Utilities w/ Connection.cs and Email.cs, added Restaurant API, added RestaurantReviewLibrary

---
+++ TermProject/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TermProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult CreateAccount()
+        {
+            return View();
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ TermProject/Controllers/RestaurantController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TermProject.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        public IActionResult CreateRestaurant()
+        {
+            return View();
+        }
+
+        public IActionResult ModifyRestaurant()
+        {
+            return View();
+        }
+
+        public IActionResult RestaurantProfile()
+        {
+            return View();
+        }
+    }
+}
