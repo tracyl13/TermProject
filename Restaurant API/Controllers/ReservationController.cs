682131989d8aaef04ff291702940983ccd8f328d
Elossiem <sunnykeyly@gmail.com>
2025-04-15 09:54:40 -0400

    Began work on RESTAPI, will start with reservations, then reviews.

---
+++ Restaurant API/Controllers/ReservationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        
+
+
+    }
+}
